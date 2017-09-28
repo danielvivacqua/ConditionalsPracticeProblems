8d36b58d306285045952923d71e75b6ae416c7bf
danielvivacqua <drvivacqua@hotmail.com>
2017-09-28 10:15:27 -0400

    Initial commit

---
+++ ConditionalsPracticeProblems/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConditionalsPracticeProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Check whether a letter is a vowel or a consonant
+            //Console.WriteLine("Please enter a letter.");
+            //string userLetter = Console.ReadLine().ToLower();
+            //char yourLetter = char.Parse(userLetter);
+
+            //if(yourLetter == 'a' || yourLetter == 'e' || yourLetter == 'i' || yourLetter == 'o' || yourLetter == 'u')
+            //{
+            //    Console.WriteLine("Your letter is a vowel");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Your letter is a consonant");
+            //}
+
+            //switch (yourLetter)
+            //{
+            //    case 'a':
+            //        Console.WriteLine("This is a vowel");
+            //        break;
+            //    case 'e':
+            //        Console.WriteLine("This is a vowel");
+            //        break;
+            //    case 'i':
+            //        Console.WriteLine("This is a vowel");
+            //        break;
+            //    case 'o':
+            //        Console.WriteLine("This is a vowel");
+            //        break;
+            //    case 'u':
+            //        Console.WriteLine("This is a vowel");
+            //        break;
+            //    default:
+            //        Console.WriteLine("This is a consonant.");
+            //        break;
+            //}
+
+            Console.WriteLine("Enter a whole number.");
+            int firstValue = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a second whole number.");
+            int secondValue = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a third whole number.");
+            int thirdValue = int.Parse(Console.ReadLine());
+
+            if (firstValue > secondValue)
+            {
+                if (firstValue > thirdValue)
+                {
+                    Console.WriteLine(firstValue + " is the larger value.");
+                }
+                else
+                {
+                    Console.WriteLine(thirdValue + " is the largest value.");
+                }
+            }
+            else if (secondValue > firstValue)
+            {
+                Console.WriteLine(secondValue + " is the larger value.");
+            }
+            else
+            {
+                Console.WriteLine("Go back to start.");
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+}
